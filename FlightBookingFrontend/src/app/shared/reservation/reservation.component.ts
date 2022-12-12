36b6d3783daf3ea4a35a8e71fd51e61f7c214949
Igor <igornedovic@gmail.com>
2022-12-12 23:52:11 +0100

    Change reservation status view is set up correctly

--- FlightBookingFrontend/src/app/shared/reservation/reservation.component.ts
+++ FlightBookingFrontend/src/app/shared/reservation/reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { Reservation, ReservationStatus } from 'src/app/models/reservation.model';
 import { AuthService } from 'src/app/services/auth.service';
@@ -8,8 +8,11 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.css']
 })
-export class ReservationComponent implements OnInit {
+export class ReservationComponent implements OnInit, AfterViewInit {
   @Input() reservation: Reservation;
+  @ViewChild('selectedStatus') selectedStatusRef: ElementRef;
+  @ViewChild('changeStatusCheckbox') changeStatusCheckboxRef: ElementRef;
+  @ViewChild('saveButton') saveButton: ElementRef;
   public reservationStatus = Object.values(ReservationStatus);
 
   constructor(public authService: AuthService) { }
@@ -17,7 +20,25 @@ export class ReservationComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.selectedStatusRef.nativeElement.disabled = true;
+    this.saveButton.nativeElement.disabled = true;
+  }
+
+  onChangeCheckbox(changeStatusCheckbox: HTMLInputElement) {
+    if (changeStatusCheckbox.checked) {
+      this.selectedStatusRef.nativeElement.disabled = false;
+      this.saveButton.nativeElement.disabled = false;
+    } else {
+      this.selectedStatusRef.nativeElement.disabled = true;
+      this.saveButton.nativeElement.disabled = true;
+    }
+  }
+
   onChangeStatus(selectedStatus: HTMLInputElement) {
     console.log(selectedStatus.value);
+    this.selectedStatusRef.nativeElement.disabled = true;
+    this.changeStatusCheckboxRef.nativeElement.checked = false;
+    this.saveButton.nativeElement.disabled = true;
   }
 }
